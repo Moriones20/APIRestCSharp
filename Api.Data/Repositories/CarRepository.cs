35f29274b7e14fad78b2a5f46c129924ddcbe4ef
Felipe Moriones <moriones06082001@gmail.com>
2023-10-18 21:54:20 -0500

    Muchos cambios

---
+++ Api.Data/Repositories/CarRepository.cs
@@ -0,0 +1,50 @@
+﻿using Api.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly MySqlConfiguration _connectionString;
+
+        public CarRepository(MySqlConfiguration connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected MySqlConnection DbConection()
+        {
+            return new MySqlConnection(_connectionString.ConnectionString);
+        }
+
+        public Task<bool> DeleteCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Car>> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Car> GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ api/Controllers/CarsController.cs
@@ -0,0 +1,68 @@
+﻿using Api.Data.Repositories;
+using Api.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly CarRepository _carRepository;
+
+        public CarsController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCars()
+        {
+            return Ok(await _carRepository.GetAllCars());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetCarDetails(int id)
+        {
+            return Ok(await _carRepository.GetDetails(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCar([FromBody] Car car)
+        {
+            if (car == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var created = await _carRepository.InsertCar(car);
+
+            return Created("created", created);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateCar([FromBody] Car car)
+        {
+            if (car == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _carRepository.UpdateCar(car);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteCar(int id)
+        {
+            await _carRepository.DeleteCar(new Car { Id = id });
+            return NoContent();
+        }
+
+    }
+}
